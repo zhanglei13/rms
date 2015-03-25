bfe26042ec9681cb9d8e1d870d6e481e4d5bf86b
Eric-UCAS <271884119@qq.com>
2015-03-25 15:32:10 +0800

    add controller

---
+++ src/com/lenovo/rms/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.lenovo.rms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+	@ResponseBody
+	public String login(){
+		System.out.println("haha");
+		return "Success";
+	} 
+	
+}
