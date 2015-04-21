bd564922028a95382b85fc0a9cfbcdc6ef9b5cc5
Eric <Eric@10.100.72.64>
2015-04-21 15:02:26 +0800

    recover

--- src/com/lenovo/rms/model/EmployeeWorkload.java
+++ src/com/lenovo/rms/model/EmployeeWorkload.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -66,7 +64,6 @@ public class EmployeeWorkload {
 
 	// Property accessors
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID", unique = true, nullable = false)
 	public Long getId() {
 		return this.id;
